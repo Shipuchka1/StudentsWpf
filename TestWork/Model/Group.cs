5a5b192e04cea93a6c3d62c76abbe758d5f88718
Наталья <natalie1995@list.ru>
2018-10-25 09:30:33 +0600

    Добавьте файлы проекта.

---
+++ TestWork/Model/Group.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TestWork.Model
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Student> Students  { get; set; }
+        public Group()
+        {
+            Students = GetStudentsForGroup();
+        }
+        public List<Student> GetStudentsForGroup()
+        {
+            return ModelService.GetAllStudents().Where(w => w.GroupId == Id).ToList();
+        }
+    }
+
+}
---
+++ TestWork/Model/ModelService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestWork.Model
+{
+    public class ModelService
+    {
+
+        public static List<Group> GetAllGroups()
+        {
+            List<Group> groups = JsonConvert.DeserializeObject<List<Group>>(File.ReadAllText("Groups.json"));
+            foreach (Group item in groups)
+            {
+                item.Students = item.GetStudentsForGroup();
+            }
+            return groups;
+        }
+
+        public static List<Student> GetAllStudents()
+        {
+            List<Student> students = JsonConvert.DeserializeObject<List<Student>>(File.ReadAllText("Students.json"));
+            return students;
+        }
+    }
+}
